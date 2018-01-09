8464ca2ca9223da6677a1c3e9918c16ac6447bce
Michelle Williams <mattie.williams31@gmail.com>
2018-01-09 00:56:05 -0500

    More CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,42 @@
 $(document).ready(function(){
   //-------------------------
+
+//Red button click
 $(".playbuttons").on("click",function(){
   var push = $(this).addClass("push");
-  console.log(push);
   setTimeout(function() {
     push.removeClass("push");
+}, 100);
+})
+
+//Select and start button click
+$(".buttons").on("click",function(){
+  var cpush = $(this).addClass("consolepush");
+  setTimeout(function(){
+    cpush.removeClass("consolepush");
+},100);
+})
+
+
+//Dpad click
+//right
+var circle = $("#circle");
+$("#pad2 i").on("click", function(){
+  circle.css("box-shadow", "inset -2px -2px 2px 1px #100d0d");
+  setTimeout(function() {
+    circle.css("box-shadow", "inset 0px -2px 2px 1px #100d0d");
 }, 200);
 })
 
-var circle = $("#circle")
+//left
+$("#pad i").on("click", function(){
 
-$("i").on("click", function(){
-  console.log("pad clicked");
+circle.css("box-shadow", "inset 2px -2px 2px 1px #100d0d");
+setTimeout(function() {
+  circle.css("box-shadow", "inset 0px -2px 2px 1px #100d0d");
+}, 200);
 })
+
+
   //-------------------------
 });
