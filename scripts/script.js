6b5ae85861166cf7c6420c2c0676d9979e63fa40
Michelle Williams <mattie.williams31@gmail.com>
2018-01-08 23:59:49 -0500

    More Life

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
   //-------------------------
 $(".playbuttons").on("click",function(){
   var push = $(this).addClass("push");
+  console.log(push);
   setTimeout(function() {
     push.removeClass("push");
 }, 200);
 })
+
+var circle = $("#circle")
 
 $("i").on("click", function(){
   console.log("pad clicked");
